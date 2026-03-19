037e38a28da16bd4de75afdc8d0d0e0708936c6a
CrystoferAT <crystofer.tre@cs.up.edu.br>
2026-03-19 13:17:11 -0300

    Finalizacao do Exercicio04

--- Atividades/Arrays/Exercicio04.java
+++ Atividades/Arrays/Exercicio04.java
@@ -6,8 +6,6 @@ public class Exercicio04 {
         int opcao = 0;
         String nome ; 
 
-        
-
             do{
                 System.out.println("------MENU------");
                 System.out.println("[1] Adicionar aluno.");
@@ -20,19 +18,46 @@ public class Exercicio04 {
                 scanner.nextLine();
                 switch (opcao) {
                     case 1: 
-                            System.out.println("------Adicionar alunos------");
-                            System.out.println("Digite [sair] para sair");
+                        System.out.println("------Adicionar alunos------");
+                        System.out.println("Digite [sair] para sair");
                             do{
                                 System.out.print("Digite o nome do aluno:");
-                                nome = scanner.nextLine();
+                                nome = scanner.nextLine().trim();
                                 if(!nome.equalsIgnoreCase("sair")){
                                     alunos.add(nome);
                                     System.out.println("Aluno adicionado com sucesso.");
-                                }
-                                    
+                                } 
                             }while (!nome.equalsIgnoreCase("sair")  ); 
+                            
                             break;
-                        
+                    case 2:
+                        for(String aluno : alunos){
+                            System.out.println(aluno);
+                        }
+                        break;
+                    case 3:
+                        System.out.print("Digite o nome do aluno para buscar:");
+                        String nomeBusca = scanner.nextLine().trim();
+                            
+                            if(alunos.contains(nomeBusca)){
+                                System.out.printf("Aluno %s esta na lista\n", nomeBusca);
+                            }else{
+                                System.out.printf("Aluno %s nao esta na lista\n", nomeBusca);
+                            }
+                            
+                            break;
+                    case 4:
+                        System.out.print("Digite o nome do aluno para ser removido:");
+                        String nomeDel = scanner.nextLine().trim();
+                            
+                            if(alunos.contains(nomeDel)){
+                                alunos.remove(nomeDel);
+                                System.out.printf("Aluno %s removido com sucesso.\n", nomeDel);
+                            }else{
+                                System.out.printf("Aluno %s nao esta na lista.\n", nomeDel);
+                            }
+                            
+                            break;    
                     default:
                         System.out.println("Opcao invalida");
                         break;
